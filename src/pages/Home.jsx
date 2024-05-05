39a3ff2415fe5cb0ffb623919ad8c7a8cda163d9
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-05-05 19:03:00 +0600

    tabes added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Carousel from "../components/Carousel";
+import TabCategories from "../components/TabCategories";
 
 
 const Home = () => {
     return (
         <div>
             <Carousel />
+            <TabCategories />
         </div>
     );
 };
