cb6de1db56dd4498ee110f2c27955770b0744ff6
mdsaifulislamrafel <mdsaifulisalmrafel@gmail.com>
2024-05-05 21:02:21 +0600

    update and delte item added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,6 +6,10 @@ import Home from "../pages/Home";
 import Login from "../pages/Authentication/Login";
 import Registration from "../pages/Authentication/Register";
 import ErrorPage from "../pages/ErrorPage";
+import JobDetails from "../pages/JobDetails";
+import AddJob from "../pages/AddJob";
+import MyPostedJobs from "../pages/MyPostedJobs";
+import UpdateJob from "../pages/UpdateJob";
 
 export const router = createBrowserRouter([
     {
@@ -24,6 +28,25 @@ export const router = createBrowserRouter([
             {
                 path: "/registration",
                 element: <Registration />
+            },
+            {
+                path: "/job/:id",
+                element: <JobDetails />,
+                loader: ({ params }) => fetch(`${import.meta.env.VITE_API_URL}/job/${params.id}`)
+            },
+            {
+                path: '/update/:id',
+                element: <UpdateJob />,
+                loader: ({ params }) =>
+                    fetch(`${import.meta.env.VITE_API_URL}/job/${params.id}`),
+            },
+            {
+                path: "/add-job",
+                element: <AddJob />
+            },
+            {
+                path: "/my-posted-jobs",
+                element: <MyPostedJobs />
             }
         ]
     },
